697de8af9fb84dede13355f5c2eff659fb3472d9
William Kamau <willikay11@gmail.com>
2020-01-07 06:24:51 +0300

    Accessed context in class components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
 import React, {Component} from "react";
+import {ThemeContext} from "../context/ThemeContext";
 
 class BookList extends Component{
+    static contextType = ThemeContext;
+
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme  = isLightTheme ? light : dark;
         return(
-            <div className="book-list">
+            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
                 <ul>
-                    <li>The way of the kings</li>
-                    <li>The name of the wind</li>
-                    <li>The final empire</li>
+                    <li style={{ background: theme.ui }}>The way of the kings</li>
+                    <li style={{ background: theme.ui }}>The name of the wind</li>
+                    <li style={{ background: theme.ui }}>The final empire</li>
                 </ul>
             </div>
         );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
 import React, {Component} from "react";
+import {ThemeContext} from "../context/ThemeContext";
+
+// Using context in a component. Benefit of this method is it can be used in both functional and class component. Another
+// benefit is that it can access multiple contexts
 
 class Navbar extends Component{
     render() {
         return(
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+                const theme  = isLightTheme ? light : dark;
+
+                return(
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                );
+            }}
+            </ThemeContext.Consumer>
         )
     }
 }
