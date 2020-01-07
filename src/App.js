c6e736d541c88c7fde4c369244faddccceeb5cd4
William Kamau <willikay11@gmail.com>
2020-01-07 06:01:27 +0300

    Added context provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ThemeContextProvider from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+        <ThemeContextProvider>
+            <Navbar />
+            <BookList />
+        </ThemeContextProvider>
     </div>
   );
 }
